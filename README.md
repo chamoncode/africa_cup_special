bf3efa3a4a79f4d0d2d0558d6a0e2af93e5f4d23
chamoncode <hicham.amchaar@ehsantechnologies.com>
2024-01-16 05:43:57 +0100

    feat: first version

---
+++ README.md
@@ -0,0 +1,46 @@
+# African Cup of Nations 2024 Results Scraper
+
+This project is a Python script that scrapes the results of the African Cup of Nations 2024 from the Eurosprt website.
+
+It is automatically refreshed (uses polling).
+
+```mermaid
+graph LR
+P(Polling2+requests) --> A[Website]
+P --> B(BeautifulSoup)
+B --> C{Data Extraction}
+C --> E{Data Processing}
+E --> F[Streamlit]
+F --> G[Dynamic Webapp]
+```
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+You need to have Python installed on your machine. You can download Python [here](https://www.python.org/downloads/).
+Tested with Python 3.12
+
+### Installing
+
+1. Clone the repository:
+    ```sh
+    git clone <repository_url>
+    ```
+2. Navigate to the project directory:
+    ```sh
+    cd <project_directory>
+    ```
+3. Install the required packages:
+    ```sh
+    pip install -r requirements.txt
+    ```
+
+## Running the Script
+
+To run the app, use the following command:
+
+```sh
+streamlit run app.py
---
+++ app.py
@@ -0,0 +1,45 @@
+"""
+This script allows you to get the results of the African Cup of Nations 2024 Round By Round
+From the Eurosport website: 
+https://www.eurosport.fr/football/coupe-d-afrique-des-nations/calendrier-resultats.shtml
+"""
+
+
+import requests
+import streamlit as st
+from bs4 import BeautifulSoup
+from polling2 import poll
+
+URL = "https://www.eurosport.fr/football/coupe-d-afrique-des-nations/calendrier-resultats.shtml"
+
+response = poll(lambda: requests.get(URL, timeout=10), step=60, poll_forever=True)
+
+# Get the HTML content from the response
+html_content = response.text
+
+# Create a BeautifulSoup object with the HTML content
+soup = BeautifulSoup(html_content, "html.parser")
+
+# Find all divs with attribute data-testid="team-match-score-atom-container"
+divs = soup.find_all("div", attrs={"data-testid": "team-match-score-atom-container"})
+
+# Make the fixtures table
+fixtures_table = []
+
+for div in divs:
+    # Get the children divs
+    children = div.findChildren("div", recursive=False)
+    # Append the fixture info to the fixtures table
+    fixtures_table.append(
+        {
+            "Team A": children[0].text,
+            "Score": f"{children[1].text} - {children[2].text}",
+            "Team B": children[3].text,
+        }
+    )
+st.dataframe(
+    fixtures_table,
+    hide_index=True,
+    height=458,
+    use_container_width=True,
+)
